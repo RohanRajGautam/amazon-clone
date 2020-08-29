6b0e77e3c4490de079286383c50d353e314214d6
rohan <rohanrajgautam@gmail.com>
2020-08-29 09:36:35 +0545

    :fire: initial commit :fire:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route path='/checkout'>
+            <h1>checkout</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>login page</h1>
+          </Route>
+          {/* This is a default route */}
+          <Route path='/'>
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
